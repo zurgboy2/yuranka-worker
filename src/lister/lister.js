5d911c60d790c099fb8a27ac8eb33799d6c52689
Naleef <mohamednaleef@gmail.com>
2025-02-15 00:58:20 +0000

    changes to lister app

--- src/lister/lister.js
+++ src/lister/lister.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import CardDisplay from './CardDisplay';
-import { useUserData } from '../userContext'; 
+import { useUserData } from '../UserContext'; 
 import { 
   TextField, Box, 
   CircularProgress, Typography, Button, Modal, Paper, Stack
