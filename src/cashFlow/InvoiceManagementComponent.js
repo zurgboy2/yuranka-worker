fb08c3d0fc4cc69826d6596deb3b9acebdf27d9f
unknown <mohamednaleef97@gmail.com>
2025-05-23 19:59:33 +0100

    Changes to invoice system

--- src/cashFlow/InvoiceManagementComponent.js
+++ src/cashFlow/InvoiceManagementComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { 
   ThemeProvider, createTheme, 
   CssBaseline, Container, Typography, Box, 
