a6142a671b53b4b0793bb0d2c8f1da92d7025f94
DilhanRubera <dilhanrubera@gmail.com>
2025-11-30 00:27:27 +0530

    feat: add worker monthly work history component for admins and store managers

--- src/roleConfig.js
+++ src/roleConfig.js
@@ -17,6 +17,7 @@ import NoticeBoardDisplay from './NoticeBoardDisplay';
 import CardSearch from './testAdmin';
 import testUser from './testUser';
 import WorkerActivityDisplay from './workerActivity';
+import WorkerMonthlyWorkHistory from './WorkerHistory';
 
 const roleConfig = {
   Worker: [
@@ -38,8 +39,9 @@ const roleConfig = {
     { component: GoogleCalendar, gridProps: { xs: 12, md: 6 }, name: 'GoogleCalendar' },
     { component: RSSFeedDisplay, gridProps: { xs: 12, md: 6 }, name: 'RSSFeedDisplay' },
     { component: NoticeBoardDisplay, gridProps: { xs: 12, md: 6 }, name: 'NoticeBoardDisplay' },
-    { component: WorkerActivityDisplay, gridProps: { xs: 12, md: 6}, name: 'WorkerActivity' },
     { component: PasswordDisplay, gridProps: { xs: 12, sm: 6 }, name: 'PasswordDisplay' },
+    { component: WorkerMonthlyWorkHistory, gridProps: { xs: 12, md: 6 }, name: 'WorkerMonthlyWorkHistory' },
+    { component: WorkerActivityDisplay, gridProps: { xs: 12, md: 6}, name: 'WorkerActivity' },
     { component: MiniTaskManager, gridProps: { xs: 12, sm: 6 }, name: 'TaskManager' },
     { component: ListerApp, gridProps: { xs: 6, md: 3 }, name: 'Lister' },
     { component: CashFlowSuperApp, gridProps: { xs: 6, md: 3 }, name: 'Accounting' },
@@ -59,8 +61,9 @@ const roleConfig = {
     { component: GoogleCalendar, gridProps: { xs: 12, md: 6 }, name: 'GoogleCalendar' },
     { component: RSSFeedDisplay, gridProps: { xs: 12, md: 6 }, name: 'RSSFeedDisplay' },
     { component: NoticeBoardDisplay, gridProps: { xs: 12, md: 6 }, name: 'NoticeBoardDisplay' },
-    { component: WorkerActivityDisplay, gridProps: { xs: 12, md: 6}, name: 'WorkerActivity' },
     { component: PasswordDisplay, gridProps: { xs: 12, sm: 6 }, name: 'PasswordDisplay' },
+    { component: WorkerMonthlyWorkHistory, gridProps: { xs: 12, md: 6 }, name: 'WorkerMonthlyWorkHistory' },
+    { component: WorkerActivityDisplay, gridProps: { xs: 12, md: 6}, name: 'WorkerActivity' },
     { component: MiniTaskManager, gridProps: { xs: 12, sm: 6 }, name: 'TaskManager' },
     { component: ListerApp, gridProps: { xs: 6, md: 3 }, name: 'Lister' },
     { component: Register, gridProps: { xs: 6, md: 3 }, name: 'Register' },
