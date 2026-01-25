acc7c2fa93f31549de077574a1cf0ee902435a0a
DilhanRubera <dilhanrubera@gmail.com>
2026-01-25 16:34:10 +0530

    feat: add outgoing invoices tab

---
+++ src/cashFlow/tabs/OutgoingTab.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Box, Tabs, Tab } from '@mui/material';
+import InvoiceListComponent from './InvoiceListComponent';
+
+const TabPanel = ({ children, value, index, ...other }) => (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`outgoing-tabpanel-${index}`}
+    aria-labelledby={`outgoing-tab-${index}`}
+    {...other}
+  >
+    {value === index && <Box sx={{ pt: 2 }}>{children}</Box>}
+  </div>
+);
+
+const OutgoingTab = () => {
+  const [subTab, setSubTab] = useState(0);
+
+  const handleSubTabChange = (event, newValue) => {
+    setSubTab(newValue);
+  };
+
+  return (
+    <Box>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={subTab} onChange={handleSubTabChange} aria-label="outgoing sub tabs">
+          <Tab label="Invoices" />
+        </Tabs>
+      </Box>
+
+      <TabPanel value={subTab} index={0}>
+        <InvoiceListComponent keyword="OP" title="Outgoing/Labour Invoices" />
+      </TabPanel>
+    </Box>
+  );
+};
+
+export default OutgoingTab;
